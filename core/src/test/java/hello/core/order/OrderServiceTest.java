778a1d936ed2308be42f4df211f674da1149e568
Nameless1004 <jjho1029@gmail.com>
2024-07-29 11:44:00 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        // 프리미티브 타입에는 null을 넣을 수 없다.
+        // 이후에 DB에 넣고 이런것들을 하면서 null 들어갈 수가 있어서.
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
